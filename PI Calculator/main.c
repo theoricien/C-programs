a212aae6d38fa5318b264f2abae358e19ac3213f
theoricien <letheoricien@hotmail.com>
2017-05-31 21:05:10 +0200

    Add files via upload

---
+++ PI Calculator/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+unsigned long long a=100000000,b=0,c=3500000,d,e=0,f[3500001],g;
+void main(){
+  for(;b-c;)f[b++]=a/5;
+  for(;d=0,g=c*2;c-=28,printf("%.8d",e+d/a),e=d%a)
+  for(b=c;  d+=f[b]*a,f[b]=d%--g,d/=g--,--b  ;d*=b);
+  getchar();
+}
